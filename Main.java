1f0cbb9fb4cd0eb0a6d4dbbd65ed68eca648c76f
Himanshuranjan30 <himanshuranjan30@gmail.com>
2021-05-06 10:53:45 +0530

    added comments

--- Main.java
+++ Main.java
@@ -16,37 +16,56 @@ class Main {
   
   public static String generateToken()
   {
-    String username="Himanshu";
-    String queryparams="cen";
+    String username="Himanshu"; //taking username or userid from request
+    String queryparams="cen"; //taking some query params
+
+
+    //encoding the username,queryparams and combining them.
     byte[] encodedusername=encoder.encode(username.getBytes());
     byte[] encodedparams=encoder.encode(queryparams.getBytes());
     byte[] combinedarray=new byte[encodedusername.length+encodedparams.length];
     System.arraycopy(encodedusername, 0, combinedarray, 0, encodedusername.length);
      System.arraycopy(encodedparams, 0, combinedarray, encodedusername.length, encodedparams.length);
     
-    md.update(combinedarray);
-    byte[] signature=md.digest(Secret.getBytes());
+    //adding Secret as a salt for computation 
+    md.update(Secret.getBytes());
 
+    //generating signature
+    byte[] signature=md.digest(combinedarray);
 
+    //generating the token
     final String token=new String(encodedusername)+'.'+new String(encodedparams)+'.'+new String(signature);
     
+    //returning the token
     return token;
      
   }
   public static void ValidateToken(String token)
   {
+    
+    //getting the token from request
     String[] tokeninfo = token.split("\\.");
     
+
+    //converting the username an params from token into bytearray
     byte[] username=tokeninfo[0].getBytes();
     byte[] params=tokeninfo[1].getBytes();
     byte[] combinedarray=new byte[username.length+params.length];
     System.arraycopy(username, 0, combinedarray, 0, username.length);
      System.arraycopy(params, 0, combinedarray, username.length, params.length);
     
-    md.update(combinedarray);
-    byte[] signature=md.digest(Secret.getBytes());
+
+    //adding the salt for computation
+    md.update(Secret.getBytes());
+
+    //generating the signature
+    byte[] signature=md.digest(combinedarray);
+
+    //getting the final token
     final String tokentocheck=new String(username)+'.'+new String(params)+'.'+new String(signature);
     System.out.println(tokentocheck);
+
+    //checking for validation
     if(generateToken().equals(tokentocheck))
     {
       System.out.print("User Validated");
